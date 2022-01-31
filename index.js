91fdec1b7bc78154ca2eee2e5339b283e55fef88
Ijay van der Walt <30903841+ijayvanderwalt@users.noreply.github.com>
2022-01-31 20:39:25 +0200

    initial commit --> part one
    setting up node, using npm, scraping a site for news related to climate change and returning a json array

---
+++ index.js
@@ -0,0 +1,34 @@
+const PORT = 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.json("Welcome to my Climate Change News API");
+});
+
+const articles = [];
+
+app.get("/news", (req, res) => {
+    axios
+        .get("https://www.theguardian.com/environment/climate-crisis")
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text();
+                const url = $(this).attr("href");
+                articles.push({
+                    title,
+                    url,
+                });
+            });
+            res.json(articles)
+        }).catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
